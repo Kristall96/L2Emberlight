e0d38263e42aea32d63354ea5dae6001f5cbaf09
Erkan Shakir <erkandev@outlook.com>
2024-11-29 18:42:01 +0200

    updated images optimized sizes and removed grand opening counter

--- script.js
+++ script.js
@@ -230,39 +230,6 @@ updateFooterTextForMobile();
 window.addEventListener("resize", updateFooterTextForMobile);
 let countdownInterval;
 
-// Function to start the countdown
-function startCountdown() {
-  const grandOpeningDate = new Date("November 1, 2024 00:00:00").getTime();
-
-  countdownInterval = setInterval(() => {
-    const now = new Date().getTime();
-    const timeLeft = grandOpeningDate - now;
-
-    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-    // Update the countdown timer elements
-    document.getElementById("days").innerText = days;
-    document.getElementById("hours").innerText = hours;
-    document.getElementById("minutes").innerText = minutes;
-    document.getElementById("seconds").innerText = seconds;
-
-    // If countdown is over
-    if (timeLeft < 0) {
-      clearInterval(countdownInterval);
-      document.getElementById("grand-opening-div").innerHTML =
-        "<h1>We're live!</h1>";
-    }
-
-    // Update the text for mobile and tablet (<=1080px)
-    handleTextUpdate(days, hours, minutes, seconds);
-  }, 1000);
-}
-
 // Function to handle text update for mobile and tablet sizes
 function handleTextUpdate(days, hours, minutes, seconds) {
   const screenWidth = window.innerWidth;
